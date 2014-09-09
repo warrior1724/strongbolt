1b62c6dbe95eea22b8fb92214cddbae1a519ed09
Thomas Césaré-Herriau <thomas.cesareherriau@gmail.com>
2014-09-09 19:34:40 +0200

    enable grant in intiialization

--- lib/strongbolt.rb
+++ lib/strongbolt.rb
@@ -21,6 +21,9 @@ ActiveRecord::Base.send :include, StrongBolt::Tenantable
 # doesn't have the UserAbilities module included
 #
 
+# Enables Grant
+Grant::Status.enable_grant
+
 # Alias the method
 Grant::User.singleton_class.send(:alias_method, :_current_user=, :current_user=)
 
