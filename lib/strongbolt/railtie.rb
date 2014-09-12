d88f010c0c0bb25b0d8dcb239e0e046eb7fd5f24
Thomas Césaré-Herriau <thomas.cesareherriau@gmail.com>
2014-09-12 19:11:37 +0200

    added devise intergrationin railtie

--- lib/strongbolt/railtie.rb
+++ lib/strongbolt/railtie.rb
@@ -5,5 +5,13 @@ module StrongBolt
     initializer "strongbolt.helpers" do
       ActionView::Base.send :include, StrongBolt::Helpers
     end
+
+    initializer "strongbolt.devise_integration" do
+      if defined? DeviseController
+        DeviseController.skip_controller_authorization
+
+        Warden::SessionSerializer.perform_without_authorization :store, :fetch, :delete
+      end
+    end
   end
 end
