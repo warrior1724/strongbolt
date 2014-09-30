2784ce5ecb14a899c5edb46ded6162566af06d4f
Thomas Césaré-Herriau <thomas.cesareherriau@gmail.com>
2014-09-30 13:39:10 +0200

    clear user auth after each

--- lib/strongbolt/rspec.rb
+++ lib/strongbolt/rspec.rb
@@ -9,10 +9,12 @@ if defined?(RSpec)
   # We load the class that overrides user behavior,
   # more convenient for tests
   require 'strongbolt/rspec/user'
-  
+
   RSpec.configure do |config|
     config.around(:each) do |example|
       StrongBolt.without_authorization &example
+      # Clear all the User authorizations that could have been defined
+      User.clear_authorizations
     end
   end
 end
