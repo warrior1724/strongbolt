cec2857180ca3ff93dfc796183fadf13fe500713
Thomas Cesare-Herriau <thomas@amg.tv>
2015-01-02 16:58:50 -0500

    added rvm config and do not override owner_id if present

--- lib/strongbolt/bolted.rb
+++ lib/strongbolt/bolted.rb
@@ -81,15 +81,6 @@ module Strongbolt
       def accessible?(action, attrs = :any)
         unbolted? || Grant::User.current_user.can?(action, self, attrs)
       end
-
-      #
-      # Returns the owner id according to what's
-      #
-      def owner_id
-        raise ModelNotOwned unless self.class.owned?
-
-        send self.class.owner_attribute
-      end
     end
     
     def self.included(receiver)
@@ -118,6 +109,17 @@ module Strongbolt
           
           granted
         end # End Grant
+
+        #
+        # Returns the owner id according to what's
+        #
+        unless instance_methods.include?(:owner_id)
+          def owner_id
+            raise ModelNotOwned unless self.class.owned?
+
+            send self.class.owner_attribute
+          end
+        end
 
       end
     end
