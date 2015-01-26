ad62c6bffa0d6e973f42003c81f9474f32df699f
Thomas Césaré-Herriau <thomas.cesareherriau@gmail.com>
2015-01-26 10:19:59 -0500

    using class constant and not name...

--- lib/strongbolt/rspec/user.rb
+++ lib/strongbolt/rspec/user.rb
@@ -2,7 +2,7 @@ require 'rspec/mocks'
 #
 # We define a can! that allows to quickly stub a user authorization
 #
-Strongbolt.user_class.class_eval do
+Strongbolt.user_class_constant.class_eval do
   def init
     RSpec::Mocks::setup(self) unless self.respond_to? :allow
   end
