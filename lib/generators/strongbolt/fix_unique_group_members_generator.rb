289df073bdcc5729a2f3f3e905a9bdd93850bb1b
Tobias Thiel <tobias011@gmail.com>
2016-05-31 13:37:24 +0200

    fix incorrect comment

--- lib/generators/strongbolt/fix_unique_group_members_generator.rb
+++ lib/generators/strongbolt/fix_unique_group_members_generator.rb
@@ -3,7 +3,8 @@ require "strongbolt/generators/migration"
 module Strongbolt
   module Generators
     #
-    # Creates a migration to fix a has many through with users tenants problem
+    # Creates a migration to add an unique index to user_groups_users,
+    # so that a user can only exist once in a group.
     #
     class FixUniqueGroupMembersGenerator < Rails::Generators::Base
       include Strongbolt::Generators::Migration
